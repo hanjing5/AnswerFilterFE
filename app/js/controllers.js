db9d9949e9209a2402bd1cea3b05a03a86acbc30
hjing <hjing@walmartlabs.com>
2014-01-07 20:23:58 -0800

    use Angular for formal data fetching

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,7 +9,10 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }])
-  .controller('NewCtrl', [function() {
-
-  }])
-  ;
+  .controller('NewCtrl', ['$scope', '$http', '$log',
+  	function($scope,$http, $log) {
+		$http.get('http://localhost:3000/answers/').success(function(data) {
+      		$scope.answers = data;
+      		$log.info(data);
+    	});
+  }]);
